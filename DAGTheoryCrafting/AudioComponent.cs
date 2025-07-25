e5603508e7a3d2874e5f192a9b806d503f11e3f2
DavidWilliamsPI <david@productiveinbox.co.uk>
2025-07-25 12:42:24 +0100

    Initial Commit

---
+++ DAGTheoryCrafting/AudioComponent.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using Audio.Processing.Pipelines;
+
+namespace Audio.Processing.Components {
+
+    public class PassThrough<T> : Processor<T, T>
+    {
+        public override T Process(T value)
+        {
+            return value;
+        }
+    }
+    public class HalfInput : Processor<double, double>
+    {
+        public override double Process(double value)
+        {
+            return value / 2;
+        }
+    }
+
+    public class AddOne : Processor<double, double>
+    {
+        public override double Process(double value)
+        {
+            return value + 1;
+        }
+    }
+
+    public class MinusOne : Processor<double, double>
+    {
+        public override double Process(double value)
+        {
+            return value - 1;
+        }
+    }
+
+    public class MultiplyByTwo : Processor<double, double>
+    {
+        public override double Process(double value)
+        {
+            return value * 2;
+        }
+    }
+
+    public class ToInteger : Processor<double, int>
+    {
+        public override int Process(double value)
+        {
+            return (int)value;
+        }
+    }
+    
+    public class ToDouble : Processor<int, double>
+    {
+        public override double Process(int value)
+        {
+            return value;
+        }
+    }
+
+    public class Average : Processor<List<double>, double>
+    {
+        public override double Process(List<double> value)
+        {
+            return value.Average();
+        }
+    }
+
+    public class AudioProcessingContext {
+        public int TrackTime { get; set; }
+        public int NoteTime { get; set; }
+        public double Frequency { get; set; }
+        public double Volume { get; set; }
+        public double Pan { get; set; }
+    }
+}
+
+namespace Audio.Processing.Pipelines {
+    public interface ISink<in T> {
+        void Consume(T value);
+    }
+
+    public interface ISource<out T> {
+        void Produce(ISink<T> consumer);
+    }
+    
+    public abstract class BaseInputBuffer<TIn, TOut> {
+        public TOut Buffer { get; set; }
+        public abstract void Add(TIn input);
+        public abstract bool IsFilled();
+    }
+
+    public class SimpleBuffer<T> : BaseInputBuffer<T, T> {
+        private bool _filled = false;
+        public SimpleBuffer() => Buffer = default(T);
+
+        public override void Add(T input)
+        {
+            Buffer = input;
+            _filled = true;
+        }
+        public override bool IsFilled() => _filled;
+    }
+
+    public class MultiBuffer<T> : BaseInputBuffer<T, List<T>> {
+        private int _parentCount = 0;
+        public MultiBuffer() => Buffer = [];
+        public override void Add(T input) => Buffer.Add(input);
+        public override bool IsFilled() => Buffer.Count == _parentCount;
+        public void AddParent() => _parentCount++;
+    }
+
+    public abstract class Processor<TIn, TOut> : ISink<TIn> {
+        protected ISink<TOut> sink;
+
+        public abstract TOut Process(TIn value);
+        public virtual void Flow(ISource<TIn> source, ISink<TOut> sink)
+        {
+            this.sink = sink;
+            source.Produce(this);
+        }
+        public void Consume(TIn value)
+        {
+            this.sink.Consume(Process(value));
+        }
+    }
+
+    public interface IInputNode<in TIn> : ISink<TIn>
+    {
+        public void AddParent();
+    }
+    
+    public class Node<TBuffer, TIn, TProcessIn, TOut> : IInputNode<TIn>, ISource<TProcessIn> where TBuffer : BaseInputBuffer<TIn, TProcessIn>, new() {
+        protected readonly TBuffer InputBuffer;
+        private readonly List<ISink<TOut>> _nextNodes = [];
+        private readonly Processor<TProcessIn, TOut>? _process;
+
+        public Node(Processor<TProcessIn, TOut>? process = null) {
+            this._process = process;
+            InputBuffer = new TBuffer();
+        }
+
+        public void Consume(TIn value) {
+            InputBuffer.Add(value);
+
+            if (_process == null || !InputBuffer.IsFilled()) return;
+            
+            foreach (var node in _nextNodes) {
+                _process.Flow(this, node);
+            }
+        }
+
+        public void Produce(ISink<TProcessIn> consumer) {
+            consumer.Consume(InputBuffer.Buffer);
+        }
+        
+        public void Connect<T>(T nextNode) where T : IInputNode<TOut> {
+            _nextNodes.Add(nextNode);
+            nextNode.AddParent();
+        }
+
+        public TProcessIn GetData() => InputBuffer.Buffer;
+        
+        public virtual void AddParent() { }
+    }
+
+    public class MultiNode<TIn, TOut> : Node<MultiBuffer<TIn>, TIn, List<TIn>, TOut>
+    {
+        public MultiNode(Processor<List<TIn>, TOut>? process = null) : base(process) { }
+        public override void AddParent()
+        {
+            InputBuffer.AddParent();
+        }
+    }
+}
---
+++ DAGTheoryCrafting/Program.cs
@@ -0,0 +1,55 @@
+﻿using Audio.Processing.Components;
+using Audio.Processing.Pipelines;
+
+var startNode = new Node<SimpleBuffer<double>, double, double, double>(new AddOne()); //2
+var secondNode = new Node<SimpleBuffer<double>, double,double, double>(new MultiplyByTwo()); //4
+var thirdNode = new Node<SimpleBuffer<double>, double, double, double>(new MultiplyByTwo()); //8
+var fourthNode = new Node<SimpleBuffer<double>, double, double, double>(new MinusOne()); //7
+var fifthNode = new Node<SimpleBuffer<double>, double, double, double>(new HalfInput()); //3.5
+var sixthNode = new Node<SimpleBuffer<double>, double, double, int>(new ToInteger()); // 3
+
+var multiNode = new MultiNode<double, double>(new Average());
+var multiNodeTwo = new MultiNode<double, double>(new Average());
+
+var outputNode = new Node<SimpleBuffer<int>, int, int, double>(new ToDouble());
+var outputNodeTwo = new Node<SimpleBuffer<double>, double, double, double>(new PassThrough<double>());
+var outputNodeThree = new Node<SimpleBuffer<double>, double, double, double>(new PassThrough<double>());
+var outputNodeFour = new Node<SimpleBuffer<double>, double, double, double>(new PassThrough<double>());
+
+
+startNode.Connect(secondNode);
+secondNode.Connect(thirdNode);
+thirdNode.Connect(fourthNode);
+fourthNode.Connect(fifthNode);
+fifthNode.Connect(sixthNode);
+sixthNode.Connect(outputNode);
+secondNode.Connect(outputNodeTwo);
+
+startNode.Connect(multiNode);
+secondNode.Connect(multiNode);
+thirdNode.Connect(multiNode);
+fourthNode.Connect(multiNode);
+fifthNode.Connect(multiNode);
+
+multiNode.Connect(outputNodeThree);
+
+outputNode.Connect(multiNodeTwo);
+outputNodeTwo.Connect(multiNodeTwo);
+outputNodeThree.Connect(multiNodeTwo);
+
+multiNodeTwo.Connect(outputNodeFour);
+
+// Run the entire pipeline
+startNode.Consume(1);
+
+var output = outputNode.GetData();
+Console.WriteLine(output);
+
+var outputTwo = outputNodeTwo.GetData();
+Console.WriteLine(outputTwo);
+
+var outputThree = outputNodeThree.GetData();
+Console.WriteLine(outputThree);
+
+var outputFour = outputNodeFour.GetData();
+Console.WriteLine(outputFour);
