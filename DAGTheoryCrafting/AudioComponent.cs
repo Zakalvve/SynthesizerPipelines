2936ec45272618e127897c2edd3a3fbf5f2c0533
David Williams <david-zakalwe-williams@outlook.com>
2025-07-29 00:13:07 +0100

    latest updates

--- DAGTheoryCrafting/AudioComponent.cs
+++ DAGTheoryCrafting/AudioComponent.cs
@@ -1,10 +1,4 @@
-﻿using Audio.Processing.Pipelines;
-
-namespace Audio.Processing.Components {
-    
-}
-
-namespace Audio.Processing.Pipelines {
+﻿namespace Audio.Processing.Pipelines {
     public class AudioProcessingContext {
         public int TrackTime { get; set; }
         public int NoteTime { get; set; }
@@ -13,36 +7,71 @@ namespace Audio.Processing.Pipelines {
         public double Pan { get; set; }
     }
     
-    public interface ISink<in T> {
+    // Interfaces
+    public interface ISink<in T>
+    {
         void Consume(T value);
     }
-    public interface ISource<out T> {
+    public interface ISource<out T>
+    {
         void Produce(ISink<T> consumer);
     }
-    public interface IConnectable<in TIn> : ISink<TIn>
+    public interface IFlushable
     {
-        public void AddParent();
+        public void Flush();
     }
-    public abstract class Processor<TIn, TOut> : ISink<TIn> {
+    public interface IFlowNode<in TIn> : ISink<TIn>
+    {
+        void AddParent<TOut>();
+    }
+    public class FlowNodeAdapter<T> : IFlowNode<T>
+    {
+        private readonly IFlowNode<object> _target;
+
+        public FlowNodeAdapter(IFlowNode<object> target)
+        {
+            _target = target;
+        }
+
+        public void Consume(T value)
+        {
+            _target.Consume(value!);
+        }
+
+        public void AddParent<TOut>()
+        {
+            _target.AddParent<TOut>();
+        }
+    }
+    public abstract class Processor<TIn, TOut> : ISink<TIn>
+    {
         private ISink<TOut> _sink;
 
         protected abstract TOut Process(TIn value);
+
         public virtual void Flow(ISource<TIn> source, ISink<TOut> sink)
         {
             _sink = sink;
             source.Produce(this);
         }
+
         public void Consume(TIn value)
         {
             _sink.Consume(Process(value));
         }
     }
-    public abstract class BaseInputBuffer<TIn, TOut> {
+
+    // Buffers
+    public abstract class BaseInputBuffer<TIn, TOut> : IFlushable
+    {
         public TOut Buffer { get; set; }
         public abstract void Add(TIn input);
         public abstract bool IsFilled();
+        public virtual void Flush() => Buffer = default(TOut);
+        public virtual void AddParent<T>() { }
     }
-    public class InputBuffer<T> : BaseInputBuffer<T, T> {
+    public class InputBuffer<T> : BaseInputBuffer<T, T>
+    {
         private bool _filled = false;
         public InputBuffer() => Buffer = default(T);
 
@@ -51,62 +80,153 @@ namespace Audio.Processing.Pipelines {
             Buffer = input;
             _filled = true;
         }
+
         public override bool IsFilled() => _filled;
+        public override void Flush() => _filled = false;
     }
-    public class MultiBuffer<T> : BaseInputBuffer<T, List<T>> {
+    public class MultiBuffer<TIn> : BaseInputBuffer<TIn, List<TIn>>
+    {
         private int _parentCount = 0;
         public MultiBuffer() => Buffer = [];
-        public override void Add(T input) => Buffer.Add(input);
-        public override bool IsFilled() => Buffer.Count == _parentCount;
-        public void AddParent() => _parentCount++;
-    }
-    public class BaseNode<TBuffer, TNodeIn, TProcessIn, TOut> : IConnectable<TNodeIn>, ISource<TProcessIn> where TBuffer : BaseInputBuffer<TNodeIn, TProcessIn>, new() {
-        protected readonly TBuffer InputBuffer;
-        private readonly List<ISink<TOut>> _nextNodes = [];
-        private readonly Processor<TProcessIn, TOut>? _process;
 
-        public BaseNode(Processor<TProcessIn, TOut>? process = null) {
-            this._process = process;
-            InputBuffer = new TBuffer();
+        public override void Add(TIn input)
+        {
+            Buffer.Add(input);
         }
+        public override bool IsFilled() => Buffer.Count == _parentCount;
+        public override void AddParent<T>() => _parentCount++;
+        public override void Flush() => Buffer.Clear();
+    }
+    public class CompositeInputBuffer<T1, TP1, TB1, T2, TP2, TB2> : BaseInputBuffer<object, (TP1, TP2)>
+        where TB1 : BaseInputBuffer<T1, TP1>, new()
+        where TB2 : BaseInputBuffer<T2, TP2>, new()
+    {
+        // Composite Input Buffer for multiple heterogeneous inputs
+        
+        private readonly TB1 _buffer1 = new();
+        private readonly TB2 _buffer2 = new();
 
-        public void Consume(TNodeIn value) {
-            InputBuffer.Add(value);
+        public override void Add(object input)
+        {
+            switch (input)
+            {
+                case T1 t1:
+                    _buffer1.Add(t1);
+                    break;
+                case T2 t2:
+                    _buffer2.Add(t2);
+                    break;
+                default:
+                    throw new InvalidOperationException($"Invalid input type: {input.GetType()}");
+            }
 
-            if (_process == null || !InputBuffer.IsFilled()) return;
-            
-            foreach (var node in _nextNodes) {
-                _process.Flow(this, node);
+            if (IsFilled())
+                Buffer = (_buffer1.Buffer, _buffer2.Buffer);
+        }
+        
+        public override void AddParent<T>()
+        {
+            if (typeof(T) == typeof(T1))
+            {
+                _buffer1.AddParent<TP1>();
+            }
+            else if (typeof(T) == typeof(T2))
+            {
+                _buffer2.AddParent<TP2>();
+            }
+            else
+            {
+                throw new InvalidOperationException($"Invalid input type: {typeof(T)}");
             }
         }
 
-        public void Produce(ISink<TProcessIn> consumer) {
-            consumer.Consume(InputBuffer.Buffer);
+        public override bool IsFilled() => _buffer1.IsFilled() && _buffer2.IsFilled();
+        public override void Flush()
+        {
+            _buffer1.Flush();
+            _buffer2.Flush();
+        }
+    }
+
+    // Nodes
+    public abstract class AbstractNode<TBuffer, TNodeIn, TProcessIn, TOut> : ISource<TProcessIn>, IFlowNode<TNodeIn>
+        where TBuffer : BaseInputBuffer<TNodeIn, TProcessIn>, new()
+    {
+        private readonly List<IFlowNode<TOut>> _nextNodes = [];
+        private readonly Processor<TProcessIn, TOut>? _process;
+        protected readonly TBuffer InputBuffer;
+
+        public AbstractNode(Processor<TProcessIn, TOut>? process = null)
+        {
+            _process = process;
+            InputBuffer = new TBuffer();
+        }
+
+        public void Consume(TNodeIn value)
+        {
+            InputBuffer.Add(value);
+            
+            // If the buffer is not filled, or no process has been assigned, we can't process this node
+            if (!InputBuffer.IsFilled() || _process == null) return;
+            
+            // Recursively traces the flow path through the graph
+            foreach (var node in _nextNodes)
+                _process.Flow(this, node);
+            
+            // On the return path flush the buffer to avoid tracing the graph twice
+            InputBuffer.Flush();
         }
         
-        public virtual void Connect<T>(T nextNode) where T : IConnectable<TOut> {
+        public void Produce(ISink<TProcessIn> consumer)
+        {
+            consumer.Consume(GetData());
+        }
+        
+        public virtual void Connect<T>(T nextNode) where T : IFlowNode<TOut>
+        {
             _nextNodes.Add(nextNode);
+            nextNode.AddParent<TOut>();
+        }
+        
+        public void ConnectToObjectSink(IFlowNode<object> nextNode)
+        {
+            _nextNodes.Add(new FlowNodeAdapter<TOut>(nextNode));
+            nextNode.AddParent<TOut>();
         }
 
         public TProcessIn GetData() => InputBuffer.Buffer;
-        
-        public virtual void AddParent() { }
+
+        // Only multi-input nodes need to have knowledge of upstream connections
+        public virtual void AddParent<T>() { }
     }
-    public class Node<TIn, TOut> : BaseNode<InputBuffer<TIn>, TIn, TIn, TOut>
+    public class Node<TIn, TOut> : AbstractNode<InputBuffer<TIn>, TIn, TIn, TOut>
     {
         public Node(Processor<TIn, TOut>? process = null) : base(process) { }
     }
-    public class MultiNode<TIn, TOut> : BaseNode<MultiBuffer<TIn>, TIn, List<TIn>, TOut>
+    public class InputNode<T> : Node<T, T>
+    {
+        public InputNode() : base(new PassThrough<T>()) { }
+    }
+    public class MultiNode<TIn, TOut> : AbstractNode<MultiBuffer<TIn>, TIn, List<TIn>, TOut>
     {
         public MultiNode(Processor<List<TIn>, TOut>? process = null) : base(process) { }
-        public override void Connect<T>(T nextNode)
+
+        public override void AddParent<T>()
         {
-            base.Connect(nextNode);
-            nextNode.AddParent();
+            InputBuffer.AddParent<T>();
         }
-        public override void AddParent()
+    }
+    public class CompositeNode<T1, T2, TOut> : AbstractNode<CompositeInputBuffer<T1, T1, InputBuffer<T1>, T2, T2, InputBuffer<T2>>, object, (T1, T2), TOut>
+    {
+        public CompositeNode(Processor<(T1, T2), TOut>? process = null) : base(process) { }
+    }
+    public class MultiCompositeNode<T1, T2, TOut> : AbstractNode<CompositeInputBuffer<T1, List<T1>, MultiBuffer<T1>, T2, List<T2>, MultiBuffer<T2>>, object, (List<T1>, List<T2>), TOut>
+    {
+        public MultiCompositeNode(Processor<(List<T1>, List<T2>), TOut>? process = null) : base(process) { }
+
+        public override void AddParent<T>()
         {
-            InputBuffer.AddParent();
+            InputBuffer.AddParent<T>();
         }
     }
 
@@ -165,6 +285,60 @@ namespace Audio.Processing.Pipelines {
             protected override double Process(List<double> value)
             {
                 return value.Average();
+            }
+        }
+
+        public class AddTenWhenInputOn : Processor<(bool, double), double>
+        {
+            protected override double Process((bool, double) value)
+            {
+                return value.Item1 ? value.Item2 + 10 : value.Item2;
+            }
+        }
+        
+        public class AdsrSimple : Processor<(bool, double), double>
+        {
+            private int _counter;
+            protected override double Process((bool, double) value)
+            {
+                var (trigger, v) = value;
+
+                if (trigger)
+                    _counter = 100;
+                
+                if (_counter > 0)
+                    _counter--;
+                
+                return _counter > 0 ? v : 0;
+            }
+        }
+
+        public class Oscillator : Processor<(int, double), double>
+        {
+            protected override double Process((int, double) value)
+            {
+                var (t, f) = value;
+                return Math.Sin(2 * Math.PI * f * t / 44200);
+            }
+        }
+
+        public class MultiCompositeProcess : Processor<(List<int>, List<double>), double>
+        {
+            protected override double Process((List<int>, List<double>) value)
+            {
+                var (ints, doubles) = value;
+                int largest = Math.Max(ints.Count, doubles.Count);
+                double total = 0;
+
+                for (int i = 0; i < largest; i++)
+                {
+                    int lhs = ints.Count > i ? ints[i] : 0;
+                    double rhs = doubles.Count > i ? doubles[i] : 0;
+                    
+                    total += lhs * rhs;
+                }
+                
+                return total;
             }
         }
     #endregion Processors
--- DAGTheoryCrafting/Program.cs
+++ DAGTheoryCrafting/Program.cs
@@ -1,4 +1,5 @@
-﻿using Audio.Processing.Pipelines;
+﻿using System.Diagnostics;
+using Audio.Processing.Pipelines;
 
 var startNode = new Node<double, double>(new AddOne()); //2
 var secondNode = new Node<double, double>(new MultiplyByTwo()); //4
@@ -22,6 +23,7 @@ thirdNode.Connect(fourthNode);
 fourthNode.Connect(fifthNode);
 fifthNode.Connect(sixthNode);
 sixthNode.Connect(outputNode);
+
 secondNode.Connect(outputNodeTwo);
 
 startNode.Connect(multiNode);
@@ -42,13 +44,169 @@ multiNodeTwo.Connect(outputNodeFour);
 startNode.Consume(1);
 
 var output = outputNode.GetData();
+Debug.Assert(output == 3);
 Console.WriteLine(output);
 
 var outputTwo = outputNodeTwo.GetData();
+Debug.Assert(outputTwo == 4);
 Console.WriteLine(outputTwo);
 
 var outputThree = outputNodeThree.GetData();
+Debug.Assert(outputThree == 4.9);
 Console.WriteLine(outputThree);
 
 var outputFour = outputNodeFour.GetData();
+Debug.Assert(outputFour == 3.966666666666667);
 Console.WriteLine(outputFour);
+
+
+var inputNodeOne = new Node<bool, bool>(new PassThrough<bool>());
+var inputNodeTwo = new Node<double, double>(new PassThrough<double>());
+
+var compositeNode = new CompositeNode<bool, double, double>(new AddTenWhenInputOn());
+
+var outputNodeFive = new Node<double, double>();
+
+inputNodeOne.ConnectToObjectSink(compositeNode);
+inputNodeTwo.ConnectToObjectSink(compositeNode);
+
+compositeNode.Connect(outputNodeFive);
+
+inputNodeOne.Consume(true);
+inputNodeTwo.Consume(10);
+
+var outputFive = outputNodeFive.GetData();
+Debug.Assert(outputFive == 20);
+Console.WriteLine(outputFive);
+
+var timeNode = new Node<int, int>(new PassThrough<int>());
+
+var aFrequencyNode = new InputNode<double>();
+var bFrequencyNode = new InputNode<double>();
+var cFrequencyNode = new InputNode<double>();
+var dFrequencyNode = new InputNode<double>();
+var eFrequencyNode = new InputNode<double>();
+var fFrequencyNode = new InputNode<double>();
+var gFrequencyNode = new InputNode<double>();
+
+var aOscillatorNode = new CompositeNode<int, double, double>(new Oscillator());
+var bOscillatorNode = new CompositeNode<int, double, double>(new Oscillator());
+var cOscillatorNode = new CompositeNode<int, double, double>(new Oscillator());
+var dOscillatorNode = new CompositeNode<int, double, double>(new Oscillator());
+var eOscillatorNode = new CompositeNode<int, double, double>(new Oscillator());
+var fOscillatorNode = new CompositeNode<int, double, double>(new Oscillator());
+var gOscillatorNode = new CompositeNode<int, double, double>(new Oscillator());
+
+var aInputNode = new InputNode<bool>();
+var bInputNode = new InputNode<bool>();
+var cInputNode = new InputNode<bool>();
+var dInputNode = new InputNode<bool>();
+var eInputNode = new InputNode<bool>();
+var fInputNode = new InputNode<bool>();
+var gInputNode = new InputNode<bool>();
+
+var aDynamicsNode = new CompositeNode<bool, double, double>(new AdsrSimple());
+var bDynamicsNode = new CompositeNode<bool, double, double>(new AdsrSimple());
+var cDynamicsNode = new CompositeNode<bool, double, double>(new AdsrSimple());
+var dDynamicsNode = new CompositeNode<bool, double, double>(new AdsrSimple());
+var eDynamicsNode = new CompositeNode<bool, double, double>(new AdsrSimple());
+var fDynamicsNode = new CompositeNode<bool, double, double>(new AdsrSimple());
+var gDynamicsNode = new CompositeNode<bool, double, double>(new AdsrSimple());
+
+var mixerNode = new MultiNode<double, double>(new Average());
+var outputNodeSix = new Node<double, double>();
+
+timeNode.ConnectToObjectSink(aOscillatorNode);
+timeNode.ConnectToObjectSink(bOscillatorNode);
+timeNode.ConnectToObjectSink(cOscillatorNode);
+timeNode.ConnectToObjectSink(dOscillatorNode);
+timeNode.ConnectToObjectSink(eOscillatorNode);
+timeNode.ConnectToObjectSink(fOscillatorNode);
+timeNode.ConnectToObjectSink(gOscillatorNode);
+
+aFrequencyNode.ConnectToObjectSink(aOscillatorNode);
+bFrequencyNode.ConnectToObjectSink(bOscillatorNode);
+cFrequencyNode.ConnectToObjectSink(cOscillatorNode);
+dFrequencyNode.ConnectToObjectSink(dOscillatorNode);
+eFrequencyNode.ConnectToObjectSink(eOscillatorNode);
+fFrequencyNode.ConnectToObjectSink(fOscillatorNode);
+gFrequencyNode.ConnectToObjectSink(gOscillatorNode);
+
+aOscillatorNode.ConnectToObjectSink(aDynamicsNode);
+bOscillatorNode.ConnectToObjectSink(bDynamicsNode);
+cOscillatorNode.ConnectToObjectSink(cDynamicsNode);
+dOscillatorNode.ConnectToObjectSink(dDynamicsNode);
+eOscillatorNode.ConnectToObjectSink(eDynamicsNode);
+fOscillatorNode.ConnectToObjectSink(fDynamicsNode);
+gOscillatorNode.ConnectToObjectSink(gDynamicsNode);
+
+aInputNode.ConnectToObjectSink(aDynamicsNode);
+bInputNode.ConnectToObjectSink(bDynamicsNode);
+cInputNode.ConnectToObjectSink(cDynamicsNode);
+dInputNode.ConnectToObjectSink(dDynamicsNode);
+eInputNode.ConnectToObjectSink(eDynamicsNode);
+fInputNode.ConnectToObjectSink(fDynamicsNode);
+gInputNode.ConnectToObjectSink(gDynamicsNode);
+
+aDynamicsNode.Connect(mixerNode);
+bDynamicsNode.Connect(mixerNode);
+cDynamicsNode.Connect(mixerNode);
+dDynamicsNode.Connect(mixerNode);
+eDynamicsNode.Connect(mixerNode);
+fDynamicsNode.Connect(mixerNode);
+gDynamicsNode.Connect(mixerNode);
+
+mixerNode.Connect(outputNodeSix);
+
+for (int i = 0; i < 338; i++)
+{
+    timeNode.Consume(i);
+    
+    aFrequencyNode.Consume(110.00);   // A2
+    bFrequencyNode.Consume(123.47);   // B2
+    cFrequencyNode.Consume(130.81);   // C3
+    dFrequencyNode.Consume(146.83);   // D3
+    eFrequencyNode.Consume(164.81);   // E3
+    fFrequencyNode.Consume(174.61);   // F3
+    gFrequencyNode.Consume(196.00);   // G3
+    
+    var index = (i / 50) % 7;
+
+    aInputNode.Consume(index == 0);
+    bInputNode.Consume(index == 1);
+    cInputNode.Consume(index == 2);
+    dInputNode.Consume(index == 3);
+    eInputNode.Consume(index == 4);
+    fInputNode.Consume(index == 5);
+    gInputNode.Consume(index == 6);
+    
+    var outputSix = outputNodeSix.GetData();
+    Console.WriteLine(outputSix);
+}
+
+
+var intInputNodeOne = new InputNode<int>();
+var intInputNodeTwo = new InputNode<int>();
+
+var doubleInputNodeOne = new InputNode<double>();
+var doubleInputNodeTwo = new InputNode<double>();
+
+
+var multiCompositeNode = new MultiCompositeNode<int, double, double>(new MultiCompositeProcess());
+
+var outputNodeSeven = new Node<double, double>();
+
+intInputNodeOne.ConnectToObjectSink(multiCompositeNode);
+intInputNodeTwo.ConnectToObjectSink(multiCompositeNode);
+doubleInputNodeOne.ConnectToObjectSink(multiCompositeNode);
+doubleInputNodeTwo.ConnectToObjectSink(multiCompositeNode);
+multiCompositeNode.Connect(outputNodeSeven);
+
+intInputNodeOne.Consume(1);
+intInputNodeTwo.Consume(2);
+doubleInputNodeOne.Consume(3.0);
+doubleInputNodeTwo.Consume(4.0);
+
+var outputSeven = outputNodeSeven.GetData();
+Debug.Assert(outputSeven == 11);
+Console.WriteLine(outputSeven);
